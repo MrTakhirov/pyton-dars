224f4a356497a93de1c61ad49e55ba28da28ca6e
Mr Takhirov <161920546+MrTakhirov@users.noreply.github.com>
2024-03-02 13:25:48 +0500

    tugadi
    pyton

---
+++ index/inde.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# narhlar = ["12000","22500", "23456", "9800", "5600","9934", "32874"]
+# arzon = min(narhlar)
+# qimmat = max(narhlar)
+# jami = sum(narhlar)
+# print("Eng arzon narh" , arzon ,".Eng qimmat narh", qimmat, ".Jami narh" , )
+
+# mehmonlar = ["Ali", "Vali", "Bobur","MuhammadYusuf","Muhammadali","Nurillo"]
+# for mehmon in mehmonlar:
+#     print("Salom", mehmon)
+
+
+##########
+
+
+# dostlar = []
+# print("5 ta eng yaqin dostingizni kim?")
+# for n in range(5):
+#     dostlar.append(input(f"{n+1} Dostingizni ismini kiriting"))
+# print(dostlar)
+
+
+
+############
+
+
+# hisob = (0)
+# mb = ("60000MB")
+# daqiqa = ("Cheksiz")
+# sms = ("3500SMS")
+
+
+# hisob = print(int(input("Hisobingizni kiriting: ")))
+
+# if hisob < 80000: 
+#     print("Sizning balansingiz:" , hisob ," Megabayitingiz:", mb , ", Daqiqangiz:" , daqiqa , ", Smslaringiz:" , sms)
+# else:
+    
+#     print("hisobingizda mablag yetarli emas")
+
+#############
+
+# menu = ['osh', 'qozonkabob', 'shashlik', 'norin', 'somsa']
+# ovqat = input("Nima ovqat yeysiz> ")
+
+# if ovqat.lower() in menu:
+#     print("Buyurtma qabul qilindi!")
+# else:
+#     print("Afsuski buyurtma topilmadi!")
